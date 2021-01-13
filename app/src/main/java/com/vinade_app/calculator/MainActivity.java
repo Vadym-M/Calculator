16a94114e277844179e1f5361ac888ca0aa9cbe3
Vadym-Muntianov <59168867+Vadym-Muntianov@users.noreply.github.com>
2021-01-13 14:54:08 +0200

    MainCalc

--- app/src/main/java/com/vinade_app/calculator/MainActivity.java
+++ app/src/main/java/com/vinade_app/calculator/MainActivity.java
@@ -9,38 +9,98 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    Button test;
+
     TextView operationField;
-    TextView resultField; // текстовое поле для вывода результата
-    EditText numberField;   // поле для ввода числа
-   // TextView operationField;    // текстовое поле для вывода знака операции
-    Double operand = null;  // операнд операции
-    String lastOperation = "="; // последняя операция
+    TextView resultField;
+    EditText numberField;
+    Double operand = null;
+    String lastOperation = "=";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-
-    test = findViewById(R.id.btn9);
+        resultField =(TextView) findViewById(R.id.resultField);
+        numberField = (EditText) findViewById(R.id.numberField);
+        operationField = (TextView) findViewById(R.id.operationField);
     operationField = findViewById(R.id.operationField);
-    test.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            //operationField.setText(test);
-        }
-    });
-}
 
+
+}
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putString("OPERATION", lastOperation);
+        if(operand!=null)
+            outState.putDouble("OPERAND", operand);
+        super.onSaveInstanceState(outState);
+    }
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        lastOperation = savedInstanceState.getString("OPERATION");
+        operand= savedInstanceState.getDouble("OPERAND");
+        resultField.setText(operand.toString());
+        operationField.setText(lastOperation);
+    }
     public void onNumberClick(View view) {
-        Button button = (Button)view;
+        Button button = (Button) view;
         numberField.append(button.getText());
 
-        if(lastOperation.equals("=") && operand!=null){
+        if (lastOperation.equals("=") && operand != null) {
             operand = null;
         }
     }
 
     public void onOperationClick(View view) {
+        Button button = (Button)view;
+        String op = button.getText().toString();
+        String number = numberField.getText().toString();
+
+        if(number.length()>0){
+            number = number.replace(',', '.');
+            try{
+                performOperation(Double.valueOf(number), op);
+            }catch (NumberFormatException ex){
+                numberField.setText("");
+            }
+        }
+        lastOperation = op;
+        operationField.setText(lastOperation);
+    }
+    private void performOperation(Double number, String operation){
+
+        
+        if(operand ==null){
+            operand = number;
+        }
+        else{
+            if(lastOperation.equals("=")){
+                lastOperation = operation;
+            }
+            switch(lastOperation){
+                case "=":
+                    operand =number;
+                    break;
+                case "/":
+                    if(number==0){
+                        operand =0.0;
+                    }
+                    else{
+                        operand /=number;
+                    }
+                    break;
+                case "*":
+                    operand *=number;
+                    break;
+                case "+":
+                    operand +=number;
+                    break;
+                case "-":
+                    operand -=number;
+                    break;
+            }
+        }
+        resultField.setText(operand.toString().replace('.', ','));
+        numberField.setText("");
     }
 
 
