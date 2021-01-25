e251f28f64575a255af2d763442b8a8f52c8f42e
Vadym-Muntianov <59168867+Vadym-Muntianov@users.noreply.github.com>
2021-01-25 19:33:29 +0200

    test

---
+++ app/src/main/java/com/vinade_app/calculator/MainActivity2.java
@@ -0,0 +1,38 @@
+package com.vinade_app.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Button tekstBtn = findViewById(R.id.button);
+        final TextView Wynik =  findViewById(R.id.Wynik);
+
+        tekstBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                EditText WagaPole= findViewById(R.id.Waga);
+                String WagaString=WagaPole.getText().toString();
+
+                EditText MililitryPole = findViewById(R.id.Mililitry);
+                String MililitryString = MililitryPole.getText().toString();
+
+                Integer Mililitry = Integer.parseInt(MililitryString);
+                Integer Waga = Integer.parseInt(WagaString);
+                Wynik.setText(String.valueOf(Mililitry*Waga));
+
+            }
+        });
+    }
+}
