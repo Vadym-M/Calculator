4b4eba05bdd7cbe3b128e3892d73a7d0a38161a9
maciejk_code <kwiatkowskimaciej1@gmail.com>
2021-01-27 18:33:09 +0100

    Checkboxes KcalCalc_rev2

--- app/src/main/java/com/vinade_app/calculator/kalkulator_kcal.java
+++ app/src/main/java/com/vinade_app/calculator/kalkulator_kcal.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -17,11 +18,14 @@ public class kalkulator_kcal extends AppCompatActivity {
         Button tekstBtn = findViewById(R.id.button);
         final TextView Wynik =  findViewById(R.id.Wynik);
 
+        CheckBox checkBox1_kcal = findViewById(R.id.checkBox1_kcal);
+        final CheckBox checkBox2_kcal = findViewById(R.id.checkBox2_kcal);
+
         checkBox1_kcal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(checkBox1_kcal.isChecked()){
-                    checkBox2.setChecked(false);
+                    checkBox2_kcal.setChecked(false);
                 }
             }
         });
@@ -30,7 +34,7 @@ public class kalkulator_kcal extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if(checkBox2_kcal.isChecked()){
-                    checkBox.setChecked(false);
+                    checkBox2_kcal.setChecked(false);
                 }
             }
         });
