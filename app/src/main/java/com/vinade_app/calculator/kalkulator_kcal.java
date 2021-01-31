6037fa7bb3756acccb6c47078d850cae80429a93
maciejk_code <kwiatkowskimaciej1@gmail.com>
2021-01-31 17:10:39 +0100

    Kcal Calc 1.0

--- app/src/main/java/com/vinade_app/calculator/kalkulator_kcal.java
+++ app/src/main/java/com/vinade_app/calculator/kalkulator_kcal.java
@@ -15,29 +15,86 @@ public class kalkulator_kcal extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kalkulator_kcal);
 
-        Button tekstBtn = findViewById(R.id.button);
-        final TextView Wynik =  findViewById(R.id.Wynik);
+        final CheckBox CheckBoxKobieta = findViewById(R.id.checkBox1_kcal);
+        final CheckBox CheckBoxMezczyzna = findViewById(R.id.checkBox2_kcal);
+        final CheckBox CheckBoxAktMala = findViewById(R.id.checkBox3);
+        final CheckBox CheckBoxAktSrednia = findViewById(R.id.checkBox4);
+        final CheckBox CheckBoxAktDuza = findViewById(R.id.checkBox5);
+        final EditText WagaKal = findViewById(R.id.WagaKcal);
+        final TextView Wynik =  findViewById(R.id.WynikKcal);
+        final double[] aktywnosc = {0};
+        final double[] bmr = {0};
 
-        CheckBox checkBox1_kcal = findViewById(R.id.checkBox1_kcal);
-        final CheckBox checkBox2_kcal = findViewById(R.id.checkBox2_kcal);
-
-        checkBox1_kcal.setOnClickListener(new View.OnClickListener() {
+        CheckBoxKobieta.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(checkBox1_kcal.isChecked()){
-                    checkBox2_kcal.setChecked(false);
+                bmr[0] = 0.9;
+                if(CheckBoxKobieta.isChecked()){
+                    CheckBoxMezczyzna.setChecked(false);
+                }
+            }
+        });
+        CheckBoxMezczyzna.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bmr[0] = 1;
+                if(CheckBoxMezczyzna.isChecked()){
+                    CheckBoxKobieta.setChecked(false);
+                }
+            }
+        });
+        CheckBoxAktDuza.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                aktywnosc[0] = 1.4;
+                if(CheckBoxAktMala.isChecked()){
+                    CheckBoxAktMala.setChecked(false);
+                }
+                if(CheckBoxAktSrednia.isChecked()){
+                    CheckBoxAktSrednia.setChecked(false);
                 }
             }
         });
 
-        checkBox2_kcal.setOnClickListener(new View.OnClickListener() {
+        CheckBoxAktSrednia.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(checkBox2_kcal.isChecked()){
-                    checkBox2_kcal.setChecked(false);
+                aktywnosc[0] = 1.2;
+                if(CheckBoxAktDuza.isChecked()){
+                    CheckBoxAktDuza.setChecked(false);
+                }
+                if(CheckBoxAktSrednia.isChecked()){
+                    CheckBoxAktMala.setChecked(false);
                 }
             }
         });
 
-    }
+        CheckBoxAktMala.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                aktywnosc[0] =  1.0;
+                if(CheckBoxAktDuza.isChecked()){
+                    CheckBoxAktDuza.setChecked(false);
+                }
+                if(CheckBoxAktSrednia.isChecked()){
+                    CheckBoxAktSrednia.setChecked(false);
+                }
+            }
+        });
+
+        Button ObliczBtn = findViewById(R.id.button2);
+
+        ObliczBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                double Waga=Integer.parseInt(WagaKal.getText().toString());
+
+                double WynikInt = Waga * 24 * bmr * aktywnosc[0];
+
+                Wynik.setText(String.valueOf(WynikInt));
+            }
+
+            });
+}
 }
